88d0c0e2a90cbe8ed2ece5ce54e274fc72742b29
Serbatinho <lserbato@gmail.com>
2024-05-27 02:36:44 -0300

    Estruturação do projeto

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <p>MKS sistemas © Todos os direitos reservados</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+const Header: React.FC = () => {
+    return (
+        <header>
+            <h1>
+                MKS sistemas
+            </h1>
+
+            <i>Carrinho de Compras</i>
+
+        </header>
+    );
+}
+
+export default Header;
