919dcd73eee47147eb8cf650d7a68b4e4b6bcc2d
Serbatinho <lserbato@gmail.com>
2024-05-28 23:45:39 -0300

    Feature Modal Compras implementada

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import Logo from "@/common/Logo";
 import CartMenu from '../CartMenu/CartMenu';
-
+import { useCart } from '@/context/CartContext';
 import styles from '../../styles/layout/components/Header/header.module.scss';
 import CartIcon from './CartIcon';
 
 const Header: React.FC = () => {
     const [isCartOpen, setIsCartOpen] = useState(false);
+    const { state } = useCart();
 
     const handleCartOpen = () => {
         setIsCartOpen(true);
@@ -16,13 +17,15 @@ const Header: React.FC = () => {
         setIsCartOpen(false);
     };
 
+    const totalItems = state.products.reduce((total, product) => total + product.quantity, 0);
+
     return (
         <header className={`${styles['header-full-content']}`}>
             <div className={`${styles['header-mid-content']}`}>
                 <Logo />
                 <button onClick={handleCartOpen} className={`${styles['header-cart-button']}`}>
                     <CartIcon />
-                    <span>Numero de itens</span>
+                    <span>{totalItems}</span>
                 </button>
             </div>
             <CartMenu isOpen={isCartOpen} onClose={handleCartClose} />
